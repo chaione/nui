fa89e6189c328e879f7194ec0e857d29dff1f365
Yuichi Fujiki <yuichi@chaione.com>
2017-04-28 16:50:20 -0500

    Created shared framework scheme for NUI.

---
+++ Demo/NUI/NUI.h
@@ -0,0 +1,19 @@
+//
+//  NUI.h
+//  NUI
+//
+//  Created by Yuichi Fujiki on 4/28/17.
+//  Copyright © 2017 Tom Benner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NUI.
+FOUNDATION_EXPORT double NUIVersionNumber;
+
+//! Project version string for NUI.
+FOUNDATION_EXPORT const unsigned char NUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NUI/PublicHeader.h>
+
+
