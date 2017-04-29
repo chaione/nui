18fb93701264e27009820c3075a1d78a3a6e875d
Yuichi Fujiki <yuichi@chaione.com>
2017-04-28 22:09:51 -0500

    Created NUI_iOS xcodeproject in order to define Carthage dependency.

---
+++ NUI_iOS/NUI_iOS/NUI_iOS.h
@@ -0,0 +1,19 @@
+//
+//  NUI_iOS.h
+//  NUI_iOS
+//
+//  Created by Yuichi Fujiki on 4/28/17.
+//  Copyright © 2017 ChaiOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NUI_iOS.
+FOUNDATION_EXPORT double NUI_iOSVersionNumber;
+
+//! Project version string for NUI_iOS.
+FOUNDATION_EXPORT const unsigned char NUI_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NUI_iOS/PublicHeader.h>
+
+
